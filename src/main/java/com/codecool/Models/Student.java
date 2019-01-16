2c7c5720f51bffabadd230395317ad8a627812c5
crabvendor <st.wyrobek@gmail.com>
2019-01-16 14:23:40 +0100

    Implement Mentor, Admin and Student model classes

--- src/main/java/com/codecool/Models/Student.java
+++ src/main/java/com/codecool/Models/Student.java
@@ -2,4 +2,18 @@ package com.codecool.Models;
 
 public class Student extends User {
 
+    private int moduleId;
+
+    public Student(int id, String firstName, String lastName, String email, String phoneNumber, String adress, int moduleId) {
+        super(id, firstName, lastName, email, phoneNumber, adress);
+        this.moduleId = moduleId;
+    }
+
+    public int getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(int moduleId) {
+        this.moduleId = moduleId;
+    }
 }
